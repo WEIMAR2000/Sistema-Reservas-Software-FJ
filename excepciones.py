4a319f1eaca2f41bbdc8aabf9de4419fc5e0bf8f
WEIMAR2000 <wtorresro@unadvirtual.edu.co>
2026-05-08 11:03:41 -0500

    Add custom exception classes for error handlingExcepciones personalizadas implementadas

--- excepciones.py
+++ excepciones.py
@@ -1,2 +1,29 @@
-class ErrorReserva(Exception):
-    pass
+# Excepción general del sistema
+class ErrorSistema(Exception):
+
+    def __init__(self, mensaje):
+        super().__init__(mensaje)
+
+
+
+# Excepción para clientes inválidos
+class ErrorCliente(ErrorSistema):
+
+    def __init__(self, mensaje):
+        super().__init__(mensaje)
+
+
+
+# Excepción para servicios inválidos
+class ErrorServicio(ErrorSistema):
+
+    def __init__(self, mensaje):
+        super().__init__(mensaje)
+
+
+
+# Excepción para reservas inválidas
+class ErrorReserva(ErrorSistema):
+
+    def __init__(self, mensaje):
+        super().__init__(mensaje)
